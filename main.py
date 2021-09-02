5daa0ab31494fce64395697f661b536ef4317856
root <root@production-builder.us-west1-b.c.railway-infra.internal>
2021-09-02 02:31:18 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from player.videoplayer import app
+
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="player"),
+)
+
+print(
+    """
+Bot Started!
+Join @TGBOTSXD & @TGBOTZXD
+"""
+)
+bot.start()
+app.start()
+idle()
